1f3130076c2cf08a180f17d6085fd1b99bbb09ec
Giovanni Campeol <91201182+twoojoo@users.noreply.github.com>
2023-06-23 10:29:21 +0200

    Added types for logger and options + some primitives

--- digest-fetch-src.d.ts
+++ digest-fetch-src.d.ts
@@ -1,6 +1,6 @@
 export class DigestClient {
     static parse(...args: any[]): string;
-    constructor(user: any, password: any, options?: {});
+    constructor(user: string, password: string, options?: DigestClientOptions);
     user: any;
     hashFunc: any;
     password: any;
@@ -18,7 +18,7 @@ export class DigestClient {
     basic: any;
     getClient(): Promise<typeof fetch | typeof import("node-fetch").default>;
     _client: typeof import("node-fetch").default;
-    fetch(url: any, options?: {}): Promise<Response | import("node-fetch").Response>;
+    fetch(url: string, options?: {}): Promise<Response | import("node-fetch").Response>;
     addBasicAuth(options?: {}): {
         headers: {
             Authorization: string;
@@ -32,4 +32,23 @@ export class DigestClient {
     parseQop(rawAuth: any): "auth" | "auth-int";
     makeNonce(): string;
 }
+
+export type Algorithm = 'MD5' | 'MD5-sess' | 'SHA-256' | 'SHA-256-sess' | 'SHA-512-256' | 'SHA-512-256-sess'
+
+export type DigestClientOptions = {
+    algorithm?: Algorithm,
+    statusCode?: number,
+    cnonceSize?: number,
+    basic?: boolean,
+    precomputeHash?: boolean,
+    logger?: DigestClientLogger
+}
+
+export interface DigestClientLogger {
+    info: (...args: any[]) => void,
+    warn: (...args: any[]) => void,
+    debug: (...args: any[]) => void,
+    [x: string]: any 
+}
+
 export default DigestClient;
